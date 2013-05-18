4cc2e19f78cf3fe4ebc4d52f493fb399c19fe81d
Shu <shuhanb@gmail.com>
2013-05-18 16:52:11 +1000

    Remove Home tab from pages

--- include/common.php
+++ include/common.php
@@ -15,7 +15,7 @@ function startValidSession() {
 
 /**
  * Redirect to given page, retaining GET query parameters
- * @param string $target 
+ * @param string $target
  */
 function redirectTo($target) {
     // Pass on query parameters
@@ -39,8 +39,36 @@ function htmlHead() {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Treasure Hunt</title>
     <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
 </head>
 <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="index.php">booty</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li><a href="current.php">Current</a></li>
+              <li><a href="hunts.php">Browse</a></li>
+              <li><a href="validate.php">Validate</a></li>
+              <li><a href="login.php">Quit</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <div id="wrapper">
+<!--
 <div id="wrapper">
     <ul id="nav">
         <li><a href="index.php">Home</a></li>
@@ -49,7 +77,7 @@ function htmlHead() {
         <li><a href="validate.php">Validate</a></li>
         <li><a href="login.php">Quit</a></li>
     </ul>
-    <div id="content">
+    <div id="content"> -->
 
 <?php
 }
@@ -57,6 +85,7 @@ function htmlHead() {
 /// Output bottom material common to each page
 function htmlFoot() {
 ?>
+    </div>
     </div>
     <div id="push"></div>
 </div>
